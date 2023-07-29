0a9cc814a4eb3214a889bda981939d1ae26bcd4e
emdevelopa <gyimahsunday@gmail.com>
2023-07-29 02:50:03 +0100

    I thank god

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const cors = require("cors");
 const mySql = require("mysql2");
 const { json } = require("body-parser");
 const cookieParser = require("cookie-parser")
-// const { createProxyMiddleware } = require('http-proxy-middleware');
 const session = require("express-session");
-// const nodemailer = require('nodemailer');
+const CryptoJS = require("crypto-js")
+
 
 const db = mySql.createPool({
     user: process.env.USERT,
@@ -26,7 +26,7 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-  }));
+}));
 
 
 
@@ -50,60 +50,19 @@ app.get("/api/getUsers", (req, res) => {
     })
 })
 
-// const transporter = nodemailer.createTransport({
-//     host: 'smtp.elasticemail.com',
-//   port: 2525,
-//   secure: false,
-//   auth: {
-//     user: 'olatunbossemma17@gmail.com',
-//     pass: 'AB94C0DE18FDF38D87FEE95E7AF8920F6018',
-//   },
-  
-//   });
-
-
-//   app.post('/send-email', (req, res) => {
-//     const { to, subject, text } = req.body;
-  
-//     const mailOptions = {
-//       from: 'olatunbossemma17@gmail.com',
-//       to,
-//       subject,
-//       text,
-//     };
-  
-//     transporter.sendMail(mailOptions, (error, info) => {
-//       if (error) {
-//         console.error('Error sending email:', error);
-//         res.status(500).send('Error sending email');
-//       } else {
-//         console.log('Email sent:', info.response);
-//         res.status(200).send('Email sent');
-//       }
-//     });
-//   });
-
-
-
-
 // Register Post
 app.post("/api/registerPost", (req, res) => {
     const { fullname, email, phone, location, pwd } = req.body;
     const sqlSelect = "SELECT * FROM users WHERE Email  = ?";
-    const sqlInsert = "INSERT INTO users(fullname,Email,Telephone,Address,Password) value(?,?,?,?,?)";
+    
     db.query(sqlSelect, [email], (error, result) => {
         if (error) {
-            console.log("error From Database");
+            console.log("error From Database", error);
         } else {
             if (result.length) {
-                res.status(200).json({ status: 200, message: "User Already exist" })
+                 res.status(200).json({status:200, message:"Email already exist"})
             } else {
-                res.status(404).json({ status: 404, message: "Success" })
-                // db.query(sqlInsert, [fullname, email, phone, location, pwd], (error, result) => {
-                //     if (error) {
-                //         console.log(error);
-                //     }
-                // })
+                res.status(404).json({ status: 404, message: "Success", ses: req.body })
             }
         }
     })
@@ -111,46 +70,119 @@ app.post("/api/registerPost", (req, res) => {
 })
 
 app.post("/api/verifyUserPost", (req, res) => {
-    const { Name, Email, Phone, Address, Password } = req.body;
-    console.log(req.body);
-    const sqlSelect = "SELECT * FROM users WHERE Email  = ?";
-    const sqlInsert = "INSERT INTO users(fullname,Email,Telephone,Address,Password) value(?,?,?,?,?)";
-    db.query(sqlSelect, [Email], (error, result) => {
+    const { token, encryptedObject,deleteToken} = req.body;
+    console.log("This is the", token);
+    let deletingToken = deleteToken;
+    const sqlSelect = "SELECT * FROM userVerification";
+    const sqlInsert = "INSERT INTO userVerification(token,encdata) value(?,?)";
+    const sqlDelete = "DELETE FROM userVerification WHERE token = ?";
+    db.query(sqlInsert, [token, encryptedObject], (error, result) => {
         if (error) {
-            console.log("error From Database");
+
+            console.log("error From Database", error);
         } else {
-            // if (result.length) {
-            //     res.status(200).json({ status: 200, message: "User Already exist" })
-            // } else {
-                res.status(404).json({ status: 404, message: "User Registered" })
-                db.query(sqlInsert, [Name, Email, Phone, Address, Password], (error, result) => {
-                    if (error) {
-                        console.log(error);
-                    }
-                })
-            // }
+            db.query(sqlSelect, (error, result) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    
+                    setTimeout(() => {
+                        deletingToken = true
+                        console.log("deleteToken =", deletingToken);
+                        db.query(sqlDelete,[token], (error, result) => {
+                                    if (error) {
+                                        console.log(error);
+                                    }else{
+                                        console.log("from the deleting",result);
+                                   
+                                    }
+                                })
+                    
+                      }, 60000 * 30);
+                     
+                    console.log(result)
+
+
+                    res.status(200).json({ status: 200, message: result })
+                }
+            }) 
         }
     })
 
 })
+app.post("/api/verifiedUserPost", (req, res) => {
+    const { Token } = req.body;
+    console.log("This is the", Token);
+    const sqlSelect = "SELECT * FROM userVerification WHERE token = ?";
+    const sqlInsert = "INSERT INTO users(fullname, Email, Telephone, Address, Password) VALUES (?, ?, ?, ?, ?)";
+    const sqlDelete = "DELETE FROM userVerification WHERE token = ?";
+
+    db.query(sqlSelect, [Token], (error, result) => {
+        if (error) {
+            console.log("SQL error", error);
+            return res.status(500).json({ error: "An error occurred while executing the SQL query" });
+        }else if(result && result.length > 0 && result[0].encdata) {
+            console.log("This is the", result[0].encdata);
+            const myencryptedObject = result[0].encdata;
+            const decryptionKey = 'myEncryptionKey';
+
+            function decryptObject(myencryptedData, key) {
+                const decryptedBytes = CryptoJS.AES.decrypt(myencryptedData, key);
+                const decryptedString = decryptedBytes.toString(CryptoJS.enc.Utf8);
+                const decryptedObject = JSON.parse(decryptedString);
+                return decryptedObject;
+            }
+
+            try {
+                const decryptedObject = decryptObject(myencryptedObject, decryptionKey);
+                console.log(JSON.parse(decryptedObject));
+                const { fullname, email, phone, location, pwd } = JSON.parse(decryptedObject);
+
+                db.query(sqlInsert, [fullname, email, phone, location, pwd], (error, result) => {
+                    if (error) {
+                        console.log("error from DB", error);
+                        return res.status(500).json({ error: "An error occurred while inserting data into the database" });
+                    } else {
+                        db.query(sqlDelete, [Token], (error, result) => {
+                            if (error) {
+                                console.log("SQL error", error);
+                                return res.status(500).json({ error: "An error occurred while deleting data from the userVerification table" });
+                            } else {
+                                console.log(result);
+                                return res.status(200).json({ message: "User data inserted and token deleted successfully" });
+                            }
+                        });
+                    }
+                });
+            } catch (error) {
+                console.log("Decryption error", error);
+                return res.status(500).json({ error: "An error occurred while decrypting the data" });
+            }
+        } else {
+            console.log("endata not present");
+            return res.status(404).json({ error: "Sorry this link has expired, kindly make your registration again" });
+        }
+    });
+});
+
 
 // Agent Register Post
-app.post("/api/agentRegister",(req,res)=>{
-    const {agentFullName,agentEmail,agentPhone,agentWhatsAppPhone,agentState,agentLGA,agentTown,agentLandMark,agentStreetAddress,agentAltAddressPerson1,agentAltAddressPerson2} = req.body;
+app.post("/api/agentRegister", (req, res) => {
+    const { agentFullName, agentEmail, agentPhone, agentWhatsAppPhone, agentState, agentLGA, agentTown, agentLandMark, agentStreetAddress, agentAltAddressPerson1, agentAltAddressPerson2 } = req.body;
     const sqlInsert = "INSERT INTO agentUsers(agentName,agentEmail,agentPhone,agentWhatsAppPhone,agentState,agentLGA,agentTown,agentLandMark,agentStreetAddress,agentAltAddressPerson1,agentAltAddressPerson2) value(?,?,?,?,?,?,?,?,?,?,?)";
     const sqlSelect = "SELECT * FROM agentUsers WHERE agentEmail  = ?"
-    db.query(sqlSelect,[agentEmail],(error,result)=>{
-        if (error){
+    db.query(sqlSelect, [agentEmail], (error, result) => {
+        if (error) {
             console.log("error From databse");
-        }else{
+        } else {
             if (result.length) {
-                res.status(200).json({staus:200,message:"Agent Already Exist"})
-            }else{
-                db.query(sqlInsert,[agentFullName,agentEmail,agentPhone,agentWhatsAppPhone,agentState,agentLGA,agentTown,agentLandMark,agentStreetAddress,agentAltAddressPerson1,agentAltAddressPerson2],(error,result)=>{
+                res.status(200).json({ staus: 200, message: "Agent Already Exist" })
+            } else {
+                db.query(sqlInsert, [agentFullName, agentEmail, agentPhone, agentWhatsAppPhone, agentState, agentLGA, agentTown, agentLandMark, agentStreetAddress, agentAltAddressPerson1, agentAltAddressPerson2], (error, result) => {
                     if (error) {
                         console.log(error);
-                    }else{
-                        res.status(404).json({status:404,message:"Agent created Successfully"})
+                    } else {
+                        res.status(404).json({ status: 404, message: "Agent created Successfully" })
                     }
                 })
                 console.log(agentFullName);
@@ -158,7 +190,7 @@ app.post("/api/agentRegister",(req,res)=>{
 
         }
     })
-    
+
     // console.log(req);
 })
 
@@ -201,60 +233,55 @@ app.post("/api/loginPost", (req, res) => {
 
 
 // Logout the user
-app.post("/api/logout",(req,res)=>{
+app.post("/api/logout", (req, res) => {
     const p = req.body.code;
     // console.log(p);
     if (p === 1) {
-      req.session.destroy();
+        req.session.destroy();
     }
 })
 
 // Upload Property
-app.post("/api/uploadProperty",(req,res)=>{
-    const {sessionEmail, availableFor, propertyPurpose, propertyType, noOfBedroom, suites, story, landType, landTypeInput, noOfFuelPumps, warehouseInput, hotelBlahBlah, state, LGA, nearestBusStop, streetName, buildingNumber, price,inspection, timeFrom, timeTo, checkboxValues,gardenAllowedRadioBtn} = req.body;
+app.post("/api/uploadProperty", (req, res) => {
+    const { sessionEmail, availableFor, propertyPurpose, propertyType, noOfBedroom, suites, story, landType, landTypeInput, noOfFuelPumps, warehouseInput, hotelBlahBlah, state, LGA, nearestBusStop, streetName, buildingNumber, price, inspection, timeFrom, timeTo, checkboxValues, gardenAllowedRadioBtn } = req.body;
     const petArrays = JSON.stringify(checkboxValues)
     console.log(gardenAllowedRadioBtn);
-    const sqlSelect =  "SELECT * FROM props WHERE AvailableFor = ? AND property_purpose = ? AND property_type = ?";
+    const sqlSelect = "SELECT * FROM props WHERE AvailableFor = ? AND property_purpose = ? AND property_type = ?";
 
     const sqlInsert = "INSERT INTO props (Email,AvailableFor,property_purpose,property_type,no_of_bedroom,no_of_suites,no_of_story,land_type,no_of_plotAcresHectres,no_of_fuelPumps,no_of_warehouse_in_square_meter,no_of_hotelrooms,state,LGA,nearestBusStop,streetName,buildingNumber,price,PetsArray,GardenAllowed,inspection,timeFrom,timeTo) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
 
-    db.query(sqlSelect,[availableFor,propertyPurpose,propertyType,noOfBedroom,suites],(error,result)=>{
+    db.query(sqlSelect, [availableFor, propertyPurpose, propertyType, noOfBedroom, suites], (error, result) => {
         if (error) {
             console.log("Database", error);
-        }else{
+        } else {
             if (result.length > 0) {
                 console.log("Property Found");
-                res.status(200).json({status:200,message:"Property Already Submited"})
+                res.status(200).json({ status: 200, message: "Property Already Submited" })
             } else {
                 console.log("Property not Found But submitted");
-                res.status(404).json({status:404,message:"Your property has been submmited successfully"})
-                db.query(sqlInsert,[sessionEmail,availableFor,propertyPurpose,propertyType,noOfBedroom,suites,story,landType,landTypeInput, noOfFuelPumps, warehouseInput, hotelBlahBlah,state,LGA,nearestBusStop,streetName,buildingNumber,price,petArrays,gardenAllowedRadioBtn,inspection,timeFrom,timeTo],(error,result)=>{
+                res.status(404).json({ status: 404, message: "Your property has been submmited successfully" })
+                db.query(sqlInsert, [sessionEmail, availableFor, propertyPurpose, propertyType, noOfBedroom, suites, story, landType, landTypeInput, noOfFuelPumps, warehouseInput, hotelBlahBlah, state, LGA, nearestBusStop, streetName, buildingNumber, price, petArrays, gardenAllowedRadioBtn, inspection, timeFrom, timeTo], (error, result) => {
                     console.log(error);
                 })
             }
         }
-    }) 
+    })
 })
 
 // Requesting For property
 
-app.post("/api/searchingForProperty",(req,res)=>{
-    const { availableFor, propertyPurpose, propertyType, noOfBedroom, suites, story, landType, landTypeInput, noOfFuelPumps, warehouseInput, hotelBlahBlah, state, LGA, nearestBusStop, budgetFrom, budgetTo, inspection, timeFrom, timeTo, searchNameContactInfo,searchPhoneCallContactInfo,searchWahtsappLineContactInfo,searchValidEmailContactInfo,searchContactInfoDropdown} = req.body;
-    
+app.post("/api/searchingForProperty", (req, res) => {
+    const { availableFor, propertyPurpose, propertyType, noOfBedroom, suites, story, landType, landTypeInput, noOfFuelPumps, warehouseInput, hotelBlahBlah, state, LGA, nearestBusStop, budgetFrom, budgetTo, inspection, timeFrom, timeTo, searchNameContactInfo, searchPhoneCallContactInfo, searchWahtsappLineContactInfo, searchValidEmailContactInfo, searchContactInfoDropdown } = req.body;
+
     // console.log(req.body);
     const sqlInsert = "INSERT INTO searchedProps (AvailableFor,property_purpose,property_type,no_of_bedroom,no_of_suites,no_of_story,land_type,no_of_plotAcresHectres,no_of_fuelPumps,no_of_warehouse_in_square_meter,no_of_hotelrooms,state,LGA,nearestBusStop,budgetFrom,budgetTo,inspection,timeFrom,timeTo,Name,PhoneCallLine,Email,WhatsappLine,HowShouldWeContactYou) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
-    res.status(200).json({status:200,message:"Submitted successfully, our AI is working on your request we will get back to you soon"})
-    db.query(sqlInsert,[availableFor, propertyPurpose, propertyType, noOfBedroom, suites, story, landType, landTypeInput, noOfFuelPumps, warehouseInput, hotelBlahBlah, state, LGA, nearestBusStop, budgetFrom, budgetTo, inspection, timeFrom, timeTo, searchNameContactInfo,searchPhoneCallContactInfo,searchWahtsappLineContactInfo,searchValidEmailContactInfo,searchContactInfoDropdown],(error,result)=>{
+    res.status(200).json({ status: 200, message: "Submitted successfully, our AI is working on your request we will get back to you soon" })
+    db.query(sqlInsert, [availableFor, propertyPurpose, propertyType, noOfBedroom, suites, story, landType, landTypeInput, noOfFuelPumps, warehouseInput, hotelBlahBlah, state, LGA, nearestBusStop, budgetFrom, budgetTo, inspection, timeFrom, timeTo, searchNameContactInfo, searchPhoneCallContactInfo, searchWahtsappLineContactInfo, searchValidEmailContactInfo, searchContactInfoDropdown], (error, result) => {
         console.log("Database", error);
     })
 })
 
-
-
-// app.get("/", (req, res) => {
-//     res.send("Fuck Node")
-// })
 
 app.listen(3002, () => {
     console.log("Server up running on port 3002");
