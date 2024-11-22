b13bc4d237f6946bed84dbcaa7febf98f808afdd
emdevelopa <gyimahsunday@gmail.com>
2024-11-22 16:33:04 +0100

    cool

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { register, login } = require('../controllers/authController');
+const { register, login, verifyToken } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.post("/verifyUserToken", verifyToken);
+
+
 
 module.exports = router;
