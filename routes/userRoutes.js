f824347453528a878c595751d0b14c6a97dfff1d
emdevelopa <gyimahsunday@gmail.com>
2024-11-23 21:24:09 +0100

    removed unnessary code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,6 @@ const authMiddleware = require("../middleware/authMiddleware");
 // View user profile
 router.get("/profile", authMiddleware, userController.getProfile);
 
-// ,"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJqb2huZG9lQGV4YW1wbGUuY29tIiwiaWF0IjoxNzMyMjk2MDcyLCJleHAiOjE3MzIyOTk2NzJ9.9ho0E7JX4PljCDNdMIUfoRdHZx6dGPkPmY7mEaqaUbY"
 
 // Update user profile  
 router.put("/profile", authMiddleware, userController.updateProfile);
